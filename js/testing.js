6bf9ba7dc5677adc5079974f06cec5a5ccead1a4
Arthur <47724914+Lilholmen@users.noreply.github.com>
2020-07-30 21:53:44 +0300

    delete task 2 in testing.js

--- js/testing.js
+++ js/testing.js
@@ -1,5 +1,19 @@
+//task 1
+/*
 console.log(
     [4,16,64,36,25]
     .filter(element => !(element % 2))
     .reduceRight((sum, element) => sum + Math.sqrt(element), 0));
-
+*/
+//task 5
+/*
+nodeLoop: for (let i = 0; i < 6; i++) { //! Эту строку можно изменить
+    for (let j = 0; j < 10; j++) {
+      if (i === 5 && j === 5) {
+        break nodeLoop;
+      }
+    
+      console.log(i, j);
+    }
+}
+*/
