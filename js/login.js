a04fd888ade5e5faca7297987ae0badd1d0d2060
Arthur <47724914+Lilholmen@users.noreply.github.com>
2020-08-01 11:41:49 +0300

    login.js complete

--- js/login.js
+++ js/login.js
@@ -1,5 +1,20 @@
-name = prompt("Who are you?", "Ваше имя");
-if (name == 'Админ') {
-
+'use strict'
+let name = prompt("Кто вы?", "Ваше имя");
+if (name === 'Админ') {
+    let passwordEntered = prompt("Введите пароль", "Пароль");
+    if (passwordEntered === 'Я главный') {
+        alert('Здравствуйте');
+    }
+    else if (passwordEntered === null) {
+        alert('Действие отменено');
+    }
+    else {
+        alert('Пароль неверный');
+    }
 }
-else ()
+else if (name === null) {
+    alert('Действие отменено');
+}
+else {
+    alert('Я вас не знаю');
+}
