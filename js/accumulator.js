2052054f15668243a7534c28ead4e6f0500808fc
Arthur <47724914+Lilholmen@users.noreply.github.com>
2020-08-04 01:13:36 +0300

    complete accumulator.js

--- js/accumulator.js
+++ js/accumulator.js
@@ -1,11 +1,18 @@
 function Accumulator(startingValue) {
     this.value = startingValue;
-    this.read()
+
+    this.read = function() {
+        do {
+            this.temp = this.value;
+            this.value += +prompt('Сколько прибавить к начальному значению?', 0);
+        } while(this.value !== this.temp);
+    };
+    this.displayValue = function() {
+        alert(this.value);
+    };
 }
 
 let accumulator = new Accumulator(1);
 
 accumulator.read();
-accumulator.read();
-alert(accumulaor.value);
-//accumaulator.showValue();
+accumulator.displayValue();
