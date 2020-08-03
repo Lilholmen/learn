87fb205e7ed50a1205d2f1f20f726365717a61f4
Arthur <47724914+Lilholmen@users.noreply.github.com>
2020-08-03 17:04:28 +0300

    complete ladder.js

---
+++ js/ladder.js
@@ -0,0 +1,21 @@
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep() {
+        alert( this.step );
+    }
+};
+
+ladder.up();
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep();
+ladder.down().down().showStep();
