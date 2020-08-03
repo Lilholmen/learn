40229fbd537ac3cd6fb275fbc961745798e46545
Arthur <47724914+Lilholmen@users.noreply.github.com>
2020-08-03 16:57:57 +0300

    complete calculator.js

---
+++ js/calculator.js
@@ -0,0 +1,19 @@
+let calculator = {
+    a: null,
+    b: null,
+    
+    read() {
+        this.a = Number(prompt('Введите первое число'));
+        this.b = Number(prompt('Введите второе число'));
+    },
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    },
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
