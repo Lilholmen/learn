57616dfee99d985cf861f85597f02610594bcb44
Arthur <47724914+Lilholmen@users.noreply.github.com>
2020-08-06 20:47:50 +0300

    array tasks in progress

--- js/testing.js
+++ js/testing.js
@@ -1,5 +1,17 @@
-function extractCurrencyValue(price) {
-    return +price.slice(1);
+"use strict";
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+        let val = arr[i];
+    
+        // удалить, если за пределами интервала
+        if (val < a || val > b) {
+          arr.splice(i, 1);
+          i--;
+        }
+      }
 }
 
-alert( extractCurrencyValue('$500') === 500);
+let arr = [5, 3, 8, 1, 4, 1, 10, 0, 55, -15, 2, 0, 0, 0, 0, 0, 3, 3];
+
+filterRangeInPlace(arr, 1, 4);
+alert( arr ); // 5,3,8,1 (без изменений)
